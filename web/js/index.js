7cca7391829b428028fa1ac5095dae6a84bd54da
JulietaPonti <jponti@fi.uba.ar>
2022-07-13 16:24:51 -0300

    cambios

--- web/js/index.js
+++ web/js/index.js
@@ -1,40 +1,40 @@
 function guardarReceta() {
 
-  let n = document.getElementById("nombre").value
-  let foto = document.getElementById("foto").value
-  let ing = document.getElementById("ingredientes").value
-  let pasos = document.getElementById("pasos").value
+    let n = document.getElementById("nombre").value
+    let foto = document.getElementById("foto").value
+    let ing = document.getElementById("ingredientes").value
+    let pasos = document.getElementById("pasos").value
+    
+    let receta = {
+        nombre: n,
+        foto:foto,
+        ingredientes: ing,
+        pasos:pasos
+    }
+    let url = "http://127.0.0.1:5000/recetas"
   
-  let receta = {
-      nombre: n,
-      foto:foto,
-      ingredientes: ing,
-      pasos:pasos
-  }
-  let url = "http://127.0.0.1:5000/recetas"
-
-  var options = {
-  body: JSON.stringify(receta),
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-     // redirect: 'follow'
-  }
-  fetch(url, options)
-      .then(function () {
-          console.log("creado")
-          alert("Grabado")
-
-          // Handle response we get from the API
-      })
-      .catch(err => {
-          //this.errored = true
-          alert("Error al grabar" )
-
-          console.error(err);
-      })
+    var options = {
+    body: JSON.stringify(receta),
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+       // redirect: 'follow'
+    }
+    fetch(url, options)
+        .then(function () {
+            console.log("creado")
+            alert("Grabado")
+  
+            // Handle response we get from the API
+        })
+        .catch(err => {
+            //this.errored = true
+            alert("Error al grabar" )
+  
+            console.error(err);
+        })
 }
- 
-function modificarProductos() {
+
+function modificarReceta() {
     let n = document.getElementById("nombre").value
     let foto = document.getElementById("foto").value
     let ing = document.getElementById("ingredientes").value
@@ -66,4 +66,32 @@ function modificarProductos() {
           alert("Error al Modificar")
       })      
 }
- 
+
+if (document.getElementById("app")) {
+    const app = new Vue({
+        el: "#app",
+        data: {
+            recetas: [],
+            errored: false,
+            loading: true
+        },
+        created() {
+            var url = 'http://127.0.0.1:5000/recetas'
+            this.fetchData(url)
+        },
+        methods: {
+            fetchData(url) {
+                fetch(url)
+                    .then(response => response.json())
+                    .then(data => {
+                        this.receta= data;
+                        this.loading = false;
+                        console.log(this.receta);
+                    })
+                    .catch(err => {
+                        this.errored = true
+                    })
+            },
+        } 
+    })
+}
